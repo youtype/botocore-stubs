48402ac7a073a9c2328ab5a4c0addb02a1d0a1f9
Vlad Emelianov <volshebnyi@gmail.com>
2022-08-31 04:24:59 +0300

    FrozenAuthToken is a NamedTuple

--- botocore-stubs/tokens.pyi
+++ botocore-stubs/tokens.pyi
@@ -1,26 +1,14 @@
 import datetime
-import json
 import logging
-import os
-import threading
-from dataclasses import dataclass
-from typing import Any, Callable, Iterable, Optional
+from typing import Any, Callable, Iterable, NamedTuple, Optional
 
-import dateutil.parser
-from botocore import UNSIGNED
-from botocore.compat import total_seconds
-from botocore.config import Config
 from botocore.credentials import JSONFileCache
-from botocore.exceptions import ClientError, InvalidConfigError, TokenRetrievalError
 from botocore.session import Session
-from botocore.utils import CachedProperty, SSOTokenLoader
-from dateutil.tz import tzutc
 
 logger: logging.Logger
 
 def create_token_resolver(session: Session) -> TokenProviderChain: ...
-@dataclass(frozen=True)
-class FrozenAuthToken:
+class FrozenAuthToken(NamedTuple):
     token: str
     expiration: Optional[datetime.datetime] = ...
 
